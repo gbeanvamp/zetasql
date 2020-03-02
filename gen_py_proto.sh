abf53acb9bf8faba8aac5080b588ea8afba4f3eb
Gregory Bean <gbean@videoamp.com>
2020-03-01 16:53:49 -0800

    Updates to python packaging for mypy stubs

--- gen_py_proto.sh
+++ gen_py_proto.sh
@@ -4,7 +4,7 @@ OUTDIR=./python
 
 ./bazel-out/host/bin/external/com_google_protobuf/protoc \
 	--python_out=$OUTDIR \
-	--python_grpc_out=$OUTDIR \
+	--mypy_out=$OUTDIR \
 	--proto_path=$OUTDIR \
     -Ibazel-bin/protos \
     bazel-bin/protos/zetasql/local_service/*.proto \
@@ -12,3 +12,11 @@ OUTDIR=./python
     bazel-bin/protos/zetasql/public/*.proto \
     bazel-bin/protos/zetasql/public/proto/*.proto \
     bazel-bin/protos/zetasql/resolved_ast/*.proto
+
+./bazel-out/host/bin/external/com_google_protobuf/protoc \
+	--python_grpc_out=$OUTDIR \
+	--proto_path=$OUTDIR \
+    -Ibazel-bin/protos \
+    bazel-bin/protos/zetasql/local_service/local_service.proto
+
+touch $OUTDIR/zetasql/py.typed
