c9e15b838c2a445fabe2897a2931568c16e07c26
Gregory Bean <gbean@videoamp.com>
2020-03-02 11:12:33 -0800

    Add ability to specify listening socket for server.

--- zetasql/server/server.cc
+++ zetasql/server/server.cc
@@ -3,16 +3,21 @@
 #include <grpcpp/server_builder.h>
 #include <grpcpp/server_posix.h>
 #include "zetasql/local_service/local_service_grpc.h"
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
 
 
 namespace zsql = ::zetasql::local_service;
+
+ABSL_FLAG(std::string, listen_address, "unix:///tmp/zetasql.sock", "server listen address");
 
 static grpc::Server* GetServer() {
     static grpc::Server* server = []() {
        static zsql::ZetaSqlLocalServiceGrpcImpl* service =
             new zsql::ZetaSqlLocalServiceGrpcImpl();
         grpc::ServerBuilder builder;
-        builder.AddListeningPort("unix:///tmp/zetasql.sock", grpc::InsecureServerCredentials());
+        ::std::string listen_address = absl::GetFlag(FLAGS_listen_address);
+        builder.AddListeningPort(listen_address, grpc::InsecureServerCredentials());
         builder.RegisterService(service);
         builder.SetMaxMessageSize(INT_MAX);
         return builder.BuildAndStart().release();
@@ -26,7 +31,9 @@ void signal_handler(int signal_num) {
     exit(signal_num);
 }
 
-int main() {
+int main(int argc, char** argv) {
+    absl::ParseCommandLine(argc, argv);
+
     auto server = GetServer();
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
